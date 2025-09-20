bc778429b4788e80c04494bf61310ea1e3781f52
Pat LaVarre <p.lavarre@ieee.org>
2025-09-20 11:06:41 -0700

    README.md: First revision
    More specifically, add first revisions of 5 Files
    
    Run ahead and copy-edit Code we like but don't yet need in from my GitHub
    
    In particular, give us first revisions of these 2 Importable Libraries =>
    
        # Amp up Import ArgParse
        # Amp up Import Traceback

--- README.md
+++ README.md
@@ -1,2 +1,182 @@
-# easter-ugg-studio
-Tic-Tac-Tuh, Chuckers, Chuss, Pung, Puckmuhn, Snuck, Tetrus, Pentuminuh, Lugohs
+<!-- omit in toc -->
+# Easter-Ugg-Studio
+
+Contents
+
+- [Quick download \& run](#quick-download--run)
+- [Which nine games are here?](#which-nine-games-are-here)
+- [Thinking ahead is allowed](#thinking-ahead-is-allowed)
+  - [What's your role in this?](#whats-your-role-in-this)
+  - [Actually you do have Linux Online, free of charge](#actually-you-do-have-linux-online-free-of-charge)
+    - [1 ) Google's first lie](#1--googles-first-lie)
+    - [2 ) Google's second lie](#2--googles-second-lie)
+    - [3 ) Google's third lie](#3--googles-third-lie)
+  - [Got Microsoft Windows or Apple macOS?](#got-microsoft-windows-or-apple-macos)
+  - [Got Python?](#got-python)
+  - [Why spell out Egg as U G G?](#why-spell-out-egg-as-u-g-g)
+- [Up online](#up-online)
+
+
+# Quick download & run
+
+These simple games do run well for me:  at iPad, at iPhone, at MacBook
+
+    git clone https://github.com/pelavarre/easter-ugg-studio.git
+    cd easter-ugg-studio/
+    python3 bin/easter-ugg-studio.py --yolo
+
+Please do [tell me](https://twitter.com/intent/tweet?text=/@PELaVarre+Easter-Ugg-Studio)
+if these games don't run well for you. We'll learn something
+
+🎮 👾
+
+
+# Which nine games are here?
+
+You'll have heard of the most famous simple board games:
+Tic-Tac-Toe, Checkers, Chess, Pong®, Snakes®, Tetris®, Pentominos, and LEGO®
+
+Here, we give you
+Tic-Tac-Tuh, Chuckers, Chuss, Pung, Puckmuhn, Snuck, Tetrus, Pentuminuh, and Lugohs
+
+Our simple games think differently.
+We fill our games with easter eggs that change the rules of the game to suit you.
+We work here to help you exercise this civil right.
+We don't know why those people out there deny you this right.
+I mean, who should be in charge? Their bots or your people?
+
+Our simple games run today pretty much the same as they did fifty years ago.
+The main thing that's changed is that today's Unicode Terminals
+invite us to draw large wide dots and squares in nine comic colors
+
+> ⚪ ⚫ 🔴 🔵 🟠 🟡 🟢 🟣 🟤 <br>
+> ⬛ ⬜ 🟥 🟦 🟧 🟨 🟩 🟪 🟫 <br>
+
+I like to feel that these Dot over Circle figures are people.  : -)
+
+These figures occupy two Terminal Columns, an even and odd, or an odd and even.
+That's new.
+The monospaced Glyphs of fifty years ago
+all occupied just 1 Column, never 2 Columns.
+Sorting these Glyphs by their Unicode Codepoints
+puts White Dots left of Black, but Black Squares left of White
+
+Different Fonts will show you the Squares as more or less Square.
+I most often test the Andale Mono 18 Font,
+which is the default Font of Apple's Terminal in its Homebrew Settings.
+
+
+# Thinking ahead is allowed
+
+
+## What's your role in this?
+
+When you try to run these games, they'll talk back,
+inviting you to
+type on your Keyboard, or
+click your Mouse, or
+touch your Touch Screen
+
+> ⌃D to quit,  Fn F1 for more help,  or ⌥-Click far from the Cursor<br>
+
+For your convenience, when next you try this again, you can ask for the Gameboard you want, by name
+> python3 bin/easter-ugg-studio.py Pung<br>
+
+We hope most of these nine simple games are coming here soon.
+Presently we have only the one game, up first
+
+
+## Actually you do have Linux Online, free of charge
+
+You do have a Linux Online, even while you feel you don't.
+It's a bit fiddly, but it comes to you free of charge
+
+It's up for you at
+> https://shell.cloud.google.com/?show=terminal
+
+Google gives it away,
+buried deep inside of your gMail,
+as part of selling their Google Cloud Shell over Microsoft Azure and Amazon Web Services
+
+Google does give it away, but first they hide it from, you behind three technical lies.
+
+### 1 ) Google's first lie
+
+This free Linux really does work if you try it,
+no matter how you choose between "Reject" and "Authorize"
+as they come and hassle you with that choice.
+Myself, I always choose "Reject".
+Why give them power they don't need.
+
+They made their English difficult to understand.
+They don't plainly tell you that you can always "Reject" them.
+They talk as if you choosing "Reject" will break them
+
+This is their first lie
+
+> Authorize Cloud Shell<br>
+> Cloud Shell needs permission to use your credentials to make Google Cloud API calls<br>
+> Click Authorize to grant permission to this and future calls<br>
+
+### 2 ) Google's second lie
+
+When you next try to close your Browser or this Browser Window,
+after opening your Cloud Shell,
+they will pop up again to spark fear in you, by speaking another lie
+
+This is their second lie
+
+> Leave site?
+> Changes you made may not be saved
+
+Just leave. They've never lost a change of mine by accident.
+I do hope they will never lose a change of yours by accident.
+By design, they do often intentionally reverse changes such as 'sudo apt install shellcheck'.
+But they've never reverted any 'git clone' of mine, never yet lost a saved game of mine
+
+They will lose an unsaved game of yours,
+if you walk away from your Browser Tab without first saving your game,
+and then they decide you've lost interest
+
+### 3 ) Google's third lie
+
+They will end by shoving a third lie,
+an advert for Google Gemini,
+into your face, as you try to leave, as if you need Google Gemini in your life.
+But you can just close that lie out too, and move on
+
+You'll have heard of America Online®, founded in 1985.
+This is like that. But this is Linux Online, and comes to you free-of-charge
+
+
+## Got Microsoft Windows or Apple macOS?
+
+You can run these Gameboards like I run them, if you have an iOS or macOS or Linux.
+You can run them differently at a Windows
+
+Inside your MacBook, Apple puts the Terminal App inside Finder > Go > Utilities
+
+To run these games at iPad or iPhone, you need to find the Linux inside.
+It's only found inside while you're online
+
+
+## Got Python?
+
+I presume you've got Python, for now is 2025.
+You can download a Python if you haven't downloaded a Python already
+
+
+## Why spell out Egg as U G G?
+
+You'll have heard of Ugg® Boots.
+Their work is completely unrelated to ours.
+But my father did once greatly please my then-12yo daughter
+by buying a pair for her
+
+
+# Up online
+
+Tested at VsCode ⇧⌘V Markdown: Open Preview <br>
+Questioned by https://twitter.com/intent/tweet?text=/@PELaVarre+Easter-Ugg-Studio <br>
+Posted as https://github.com/pelavarre/easter-ugg-studio/blob/main/README.md <br>
+<!-- Copied from:&nbsp;  git clone https://github.com/pelavarre/easter-ugg-studio.git -->
---
+++ bin/easter-ugg-studio.py
@@ -0,0 +1,388 @@
+#!/usr/bin/env python3
+
+# Pure Storage Confidential - Copyright (c) 2025 Pure Storage, Inc.
+
+r"""
+usage: easter-egg-studio.py [-h] [--yolo]
+
+give away nine classic simple Terminal games
+
+options:
+  -h, --help  show this help message and exit
+  --yolo      do what's popular now
+
+examples:
+  ./bin/easter-egg-studio.py --yolo
+"""
+
+# code reviewed by People, Black, Flake8, Mypy-Strict, & Pylance-Standard
+
+
+from __future__ import annotations  # backports new datatype syntaxes into old Pythons
+
+import __main__
+import argparse
+import bdb
+import dataclasses
+import difflib
+import os
+import pdb
+import signal
+import sys
+import textwrap
+import types
+import typing
+
+
+if not __debug__:
+    raise NotImplementedError([__debug__])  # refuses to run without live Asserts
+
+default_eq_None = None
+
+_: object
+
+
+#
+# Run from the Shell Command Line
+#
+
+
+def main() -> None:
+    """Run from the Shell Command Line, and exit into the Py Repl"""
+
+    sys.excepthook = excepthook
+
+    mc = MainClass()
+    mc.main_class_run()
+
+
+@dataclasses.dataclass(order=True)  # , frozen=True)
+class MainClass:
+
+    def __init__(self) -> None:  # todo1: needed after we add Instance Fields
+        pass
+
+    def main_class_run(self) -> None:
+
+        ns = self.parse_ugg_args()
+        assert ns.yolo, (ns.yolo, ns,)  # todo1: while no other Options declared
+
+        print("⌃D to quit,  Fn F1 for more help,  or ⌥-Click far from the Cursor<br>")
+
+        sys.stdin.read()  # waits for ⌃D or ⌃C or ⌃\
+
+
+    def parse_ugg_args(self) -> argparse.Namespace:
+        """Take in the Shell Command-Line Args"""
+
+        parser = self.compile_ugg_doc()
+
+        argv = sys.argv[1:]
+        if not argv:
+            if os.path.basename(sys.argv[0]) in ("+", "@"):
+                argv = ["--yolo"]
+
+        ns = parser.parse_args_if(argv)
+
+        return ns
+
+    def compile_ugg_doc(self) -> ArgDocParser:
+        """Declare the Options & Positional Arguments"""
+
+        doc = __main__.__doc__
+        assert doc, (doc,)
+
+        parser = ArgDocParser(doc, add_help=True)
+
+        yolo_help = "do what's popular now"
+        parser.add_argument("--yolo", action="count", help=yolo_help)
+
+        return parser
+
+
+#
+# Amp up Import ArgParse
+#
+
+
+_ARGPARSE_3_10_ = (3, 10)  # Ubuntu 2022 Oct/2021 Python 3.10
+
+
+class ArgDocParser:
+    """Scrape out Prog & Description & Epilog from Doc to form an Argument Parser"""
+
+    doc: str  # a copy of parser.format_help()
+    add_help: bool  # truthy to define '-h, --help', else not
+
+    parser: argparse.ArgumentParser  # the inner standard ArgumentParser
+    text: str  # something like the __main__.__doc__, but dedented and stripped
+    closing: str  # the last Graf of the Epilog, minus its Top Line
+
+    add_argument: typing.Callable[..., object]
+
+    def __init__(self, doc: str, add_help: bool) -> None:
+
+        self.doc = doc
+        self.add_help = add_help
+
+        text = textwrap.dedent(doc).strip()
+
+        prog = self._scrape_prog_(text)
+        description = self._scrape_description_(text)
+        epilog = self._scrape_epilog_(text, description=description)
+        closing = self._scrape_closing_(epilog)
+
+        parser = argparse.ArgumentParser(  # doesn't distinguish Closing from Epilog
+            prog=prog,
+            description=description,
+            add_help=add_help,
+            formatter_class=argparse.RawTextHelpFormatter,  # lets Lines be wide
+            epilog=epilog,
+        )
+
+        self.parser = parser
+        self.text = text
+        self.closing = closing
+
+        self.add_argument = parser.add_argument
+
+        # callers who need Options & Positional Arguments have to add them
+
+        # 'add_help=False' for needs like 'cal -h', 'df -h', 'du -h', 'ls -h', etc
+
+    #
+    # Take in the Shell Args, else print Help and exit zero or nonzero
+    #
+
+    def parse_args_if(self, args: list[str]) -> argparse.Namespace:
+        """Take in the Shell Args, else print Help and exit zero or nonzero"""
+
+        parser = self.parser
+        closing = self.closing
+
+        # Drop the "--" Shell Args Separator, if present,
+        # because 'ArgumentParser.parse_args()' without Pos Args wrongly rejects it
+
+        shargs = args
+        if args == ["--"]:  # ArgParse chokes if Sep present without Pos Args
+            shargs = list()
+
+        # Print Diffs & exit nonzero, when Arg Doc wrong
+
+        diffs = self._diff_doc_vs_format_help_()
+        if diffs:
+            if sys.version_info >= _ARGPARSE_3_10_:
+                print("\n".join(diffs))
+
+                sys.exit(2)  # exits 2 for wrong Args in Help Doc
+
+            # takes 'usage: ... [HINT ...]', rejects 'usage: ... HINT [HINT ...]'
+            # takes 'options:', rejects 'optional arguments:'
+            # takes '-F, --isep ISEP', rejects '-F ISEP, --isep ISEP'
+
+        # Print Closing & exit zero, if no Shell Args
+
+        if not args:
+            print()
+            print(closing)
+            print()
+
+            sys.exit(0)  # exits 0 after printing Closing
+
+        # Print help lines & exit zero, else return Parsed Args
+
+        ns = parser.parse_args(shargs)
+
+        return ns
+
+        # often prints help & exits zero
+
+    #
+    # Scrape out Parser, Prog, Description, Epilog, & Closing from Doc Text
+    #
+
+    def _scrape_prog_(self, text: str) -> str:
+        """Pick the Prog out of the Usage Graf that starts the Doc"""
+
+        lines = text.splitlines()
+        prog = lines[0].split()[1]  # second Word of first Line  # 'prog' from 'usage: prog'
+
+        return prog
+
+    def _scrape_description_(self, text: str) -> str:
+        """Take the first Line of the Graf after the Usage Graf as the Description"""
+
+        lines = text.splitlines()
+
+        firstlines = list(_ for _ in lines if _ and (_ == _.lstrip()))
+        docline = firstlines[1]  # first Line of second Graf
+
+        description = docline
+        if self._docline_is_skippable_(docline):
+            description = "just do it"
+
+        return description
+
+    def _scrape_epilog_(self, text: str, description: str) -> str:
+        """Take up the Lines past Usage, Positional Arguments, & Options, as the Epilog"""
+
+        lines = text.splitlines()
+
+        epilog = ""
+        for index, line in enumerate(lines):
+            if self._docline_is_skippable_(line) or (line == description):
+                continue
+
+            epilog = "\n".join(lines[index:])
+            break
+
+        return epilog  # maybe empty
+
+    def _docline_is_skippable_(self, docline: str) -> bool:
+        """Guess when a Doc Line can't be the first Line of the Epilog"""
+
+        strip = docline.rstrip()
+
+        skippable = not strip
+        skippable = skippable or strip.startswith(" ")  # includes .startswith("  ")
+        skippable = skippable or strip.startswith("usage")
+        skippable = skippable or strip.startswith("positional arguments")
+        skippable = skippable or strip.startswith("options")  # ignores "optional arguments"
+
+        return skippable
+
+    def _scrape_closing_(self, epilog: str) -> str:
+        """Pick out the last Graf of the Epilog, minus its Top Line"""
+
+        lines = epilog.splitlines()
+
+        indices = list(_ for _ in range(len(lines)) if lines[_])  # drops empty Lines
+        indices = list(_ for _ in indices if not lines[_].startswith(" "))  # finds top Lines
+
+        closing = ""
+        if indices:
+            index = indices[-1] + 1
+
+            join = "\n".join(lines[index:])  # last Graf, minus its Top Line
+            dedent = textwrap.dedent(join)
+            closing = dedent.strip()
+
+        return closing  # maybe empty
+
+    #
+    # Form Diffs from Help Doc to Parser Format_Help
+    #
+
+    def _diff_doc_vs_format_help_(self) -> list[str]:
+        """Form Diffs from Help Doc to Parser Format_Help"""
+
+        text = self.text
+        parser = self.parser
+
+        # Say where the Help Doc came from
+
+        a = text.splitlines()
+
+        basename = os.path.split(__file__)[-1]
+        fromfile = "{} --help".format(basename)
+
+        # Fetch the Parser Doc from a fitting virtual Terminal
+        # Fetch from a Black Terminal of 89 columns, not from the current Terminal width
+        # Fetch from later Python of "options:", not earlier Python of "optional arguments:"
+
+        if "COLUMNS" not in os.environ:
+
+            os.environ["COLUMNS"] = str(89)  # adds
+            try:
+                b_text = parser.format_help()
+            finally:
+                del os.environ["COLUMNS"]  # removes
+
+        else:
+
+            with_columns = os.environ["COLUMNS"]  # backs up
+            os.environ["COLUMNS"] = str(89)  # replaces
+            try:
+                b_text = parser.format_help()
+            finally:
+                os.environ["COLUMNS"] = with_columns  # restores
+
+        b = b_text.splitlines()
+
+        tofile = "ArgumentParser(...)"
+
+        # Form >= 0 Diffs from Help Doc to Parser Format_Help,
+        # but ask for lineterm="", for else the '---' '+++' '@@' Diff Control Lines end with '\n'
+
+        diffs = list(difflib.unified_diff(a=a, b=b, fromfile=fromfile, tofile=tofile, lineterm=""))
+
+        # Succeed
+
+        return diffs
+
+
+#
+# Amp up Import Traceback
+#
+# Especially when installed via:  sys.excepthook = excepthook
+#
+
+
+with_excepthook = sys.excepthook  # aliases old hook, and fails fast to chain hooks
+assert with_excepthook.__module__ == "sys", (with_excepthook.__module__,)
+assert with_excepthook.__name__ == "excepthook", (with_excepthook.__name__,)
+
+assert sys.__stderr__ is not None  # refuses to run headless
+with_stderr = sys.stderr
+
+
+assert int(0x80 + signal.SIGINT) == 130  # discloses the Nonzero Exit Code for after ⌃C SigInt
+
+
+def excepthook(  # ) -> ...:
+    exc_type: type[BaseException],
+    exc_value: BaseException,
+    exc_traceback: types.TracebackType | None,
+) -> None:
+    """Run at Process Exit, when not bypassed by raising SystemExit"""
+
+    assert exc_type is not SystemExit, (exc_type,)
+
+    # Quit now for visible cause, if KeyboardInterrupt
+
+    if exc_type is KeyboardInterrupt:
+        with_stderr.write("KeyboardInterrupt\n")
+        sys.exit(130)  # 0x80 + signal.SIGINT
+
+    if exc_type is bdb.BdbQuit:
+        with_stderr.write("BdbQuit\n")
+        sys.exit(130)  # 0x80 + signal.SIGINT  # same as for KeyboardInterrupt
+
+    # Print the Traceback, etc
+
+    print(file=with_stderr)
+    print(file=with_stderr)
+    print("ExceptHook", file=with_stderr)
+
+    with_excepthook(exc_type, exc_value, exc_traceback)
+
+    # Launch the Post-Mortem Debugger
+
+    print(">>> pdb.pm()", file=with_stderr)
+    pdb.pm()
+
+
+#
+# Run from the Shell Command Line, if not imported
+#
+
+
+if __name__ == "__main__":
+    main()
+
+
+# 3456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+
+
+# posted into:  http://git/gitweb/?p=pure_tools.git;a=tree;f=triage/scraps
+# copied from:  git clone git://git-mirror.dev.purestorage.com/pure_tools.git
